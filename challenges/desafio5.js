f2bf0094f65503a4574ce46edbd43eb33895e5f1
laujuu <lauroindcontato@gmail.com>
2022-12-15 15:49:45 -0300

    :sparkles: feat: Retorna o nome, as curtidas e vendidos dos lanches que tiveram quantidade de curtidas igual a 36 ou tenham a quantidade de vendas igual a 85

---
+++ challenges/desafio5.js
@@ -0,0 +1,9 @@
+db.produtos.find(
+    {
+      $or: [
+        { curtidas: { $eq: 36 } },
+        { vendidos: { $eq: 85 } },
+      ],
+    },
+    { _id: false, nome: true, vendidos: true, curtidas: true },
+  );
