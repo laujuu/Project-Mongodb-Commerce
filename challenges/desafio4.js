af9e250b2b9c9d696aca44224b16e33fb4d41b01
laujuu <lauroindcontato@gmail.com>
2022-12-15 15:36:24 -0300

    :sparkles: feat: Retorna os lanches que tiveram vendas maiores que 50 e menores que 100, mostrando apenas o nome e a quantidade de lanches vendidos em ordem crescente

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,8 +1,8 @@
 db.produtos.find(
-    {},
+    { vendidos: { $gt: 50, $lt: 100 } },
     {
         _id: false,
         nome: true,
         vendidos: true,
     },
-).sort({ vendidos: -1 }).limit(1);
+).sort({ vendidos: 1 });
