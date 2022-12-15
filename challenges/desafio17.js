46e25abf7c050a4a9bb2b48030246d35050a0dc2
laujuu <lauroindcontato@gmail.com>
2022-12-15 19:20:14 -0300

    :sparkles: feat: Retorna a quantidade total de produtos em uma nova coleção chamada resumoProdutos

---
+++ challenges/desafio17.js
@@ -0,0 +1,12 @@
+db.resumoProdutos.insertOne({
+  franquia: "McDonalds",
+  totalProdutos: db.produtos.count({}),
+});
+db.resumoProdutos.find(
+  {},
+  {
+    _id: false,
+    franquia: true,
+    totalProdutos: true,
+  },
+);
